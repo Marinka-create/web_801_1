33842ad49ae9985d5ded73f4df1cd5507cd6b58d
Marinka-create <Linka29@mail.ru>
2025-07-15 14:34:13 +0300

    Add files via upload

---
+++ script/aw.js
@@ -0,0 +1,63 @@
+const bigImage = document.querySelector(".big-image img");
+
+const img1 = document.querySelector("#img-1")
+const img2 = document.querySelector("#img-2")
+const img3 = document.querySelector("#img-3")
+const img4 = document.querySelector("#img-4")
+const img5 = document.querySelector("#img-5")
+const img6 = document.querySelector("#img-6")
+const img7 = document.querySelector("#img-7")
+const img8 = document.querySelector("#img-8")
+const img9 = document.querySelector("#img-9")
+const img10 = document.querySelector("#img-10")
+const img11 = document.querySelector("#img-11")
+
+
+img1.addEventListener("click", () => {
+    bigImage.src = img1.src
+})
+
+img2.addEventListener("click", () => {
+    bigImage.src = img2.src
+})
+
+img3.addEventListener("click", () => {
+    bigImage.src = img3.src
+})
+
+img4.addEventListener("click", () => {
+    bigImage.src = img4.src
+})
+
+img5.addEventListener("click", () => {
+    bigImage.src = img5.src
+})
+
+img6.addEventListener("click", () => {
+    bigImage.src = img6.src
+})
+
+img7.addEventListener("click", () => {
+    bigImage.src = img7.src
+})
+
+img8.addEventListener("click", () => {
+    bigImage.src = img8.src
+})
+
+img9.addEventListener("click", () => {
+    bigImage.src = img9.src
+})
+
+img10.addEventListener("click", () => {
+    bigImage.src = img10.src
+})
+
+img11.addEventListener("click", () => {
+    bigImage.src = img11.src
+})
+
+document.querySelector('.big-image').addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
+
